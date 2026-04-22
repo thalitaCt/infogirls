7274080d54ff374072a8074e0e40c346091ef8db
thalitaCt <costathalita685@gmail.com>
2026-04-22 20:45:32 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,6 +5,7 @@
     <title>Navbar</title>
 </head>
     <style>
+
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Style+Script&display=swap');
 
 
@@ -187,10 +188,8 @@
                 display: flex !important;
                 align-items: center;
                 justify-content: space-between;
-                padding: 15px;
+                padding: 20px;
                 width: 100%;
-                min-height: 70px;
-                box-sizing: border-box;
             }
 
 
@@ -210,12 +209,13 @@
                 text-decoration:none;
             }
             .menu-icon{
-                display:block;
+                display:none;
                 font-size:30px;
                 cursor:pointer;
-                flex-shrink: 0;
-                margin-left: 80px;
             }
+
+
+
 
                 .menu{
                     justify-self: end;
@@ -253,6 +253,9 @@
                     display: none;
                 }
 
+
+
+
                 .menu.active{
                     display:flex;
                     flex-direction: column;
@@ -260,6 +263,16 @@
                     top: 75px;
                     right: 15px;
                 }
+
+
+
+
+                .menu-icon{
+                    order: 3;
+                    display:block;
+                    margin-left: 15px;
+                }
+
 
                 .carrinho-icon {
                     order: 2;

3a5cc33daab8dc05acd15af6bea980ad98c8e835
thalitaCt <costathalita685@gmail.com>
2026-04-22 22:06:49 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -279,7 +279,7 @@
 
 
                     #numeroC {
-                        margin-right: 20px;
+                        margin-right: 40px;
                         padding: 3px;
                         margin-top: 11px;
                         font-size: 6pt;

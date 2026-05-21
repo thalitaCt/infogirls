cf390536debe240b3c64df9a4c4ce943eea85764
thalitaCt <costathalita685@gmail.com>
2026-05-21 10:08:12 -0300

    Create sidebar.php

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,42 @@
+<div class="sidebar">
+
+    <div class="logo-admin">
+        <h1>InfoGirls</h1>
+        <span>Painel Administrativo</span>
+    </div>
+
+    <nav class="menu-admin">
+
+        <a href="../admin/index.php">
+            <i class="fa-solid fa-chart-line"></i>
+            Dashboard
+        </a>
+
+        <a href="../admin/produtos/listar.php">
+            <i class="fa-solid fa-box"></i>
+            Produtos
+        </a>
+
+        <a href="../admin/pedidos/listar.php">
+            <i class="fa-solid fa-cart-shopping"></i>
+            Pedidos
+        </a>
+
+        <a href="../admin/usuarios/listar.php">
+            <i class="fa-solid fa-users"></i>
+            Usuários
+        </a>
+
+        <a href="../admin/contatos/listar.php">
+            <i class="fa-solid fa-envelope"></i>
+            Contatos
+        </a>
+
+        <a href="../logout.php">
+            <i class="fa-solid fa-right-from-bracket"></i>
+            Sair
+        </a>
+
+    </nav>
+
+</div>
